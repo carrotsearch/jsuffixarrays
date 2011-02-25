6b5a18ce76cdf61e5f81241b82fd81fdc624f900
dweiss <dawid.weiss@gmail.com>
2011-02-25 19:05:38 +0100

    JSA-1: Initial mavenization of the project, removed dependency JARs, upgraded to new guava and commons. All tests seem to pass.

--- perf/run-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir results
-. time-on-alphabet-size.sh  > results/running.logs 2>&1
-. time-on-random-input.sh  >> results/running.logs 2>&1
-. time-on-corpus-file.sh   >> results/running.logs 2>&1
---
+++ src/main/java/org/jsuffixarrays/NaiveSort.java
@@ -0,0 +1,107 @@
+package org.jsuffixarrays;
+
+import static org.jsuffixarrays.Tools.assertAlways;
+
+import org.carrot2.util.IndirectComparator;
+import org.carrot2.util.IndirectSort;
+
+import com.google.common.primitives.Ints;
+
+/**
+ * A naive implementation of suffix sorting based on primitive integer collections and
+ * custom sorting routines (quicksort).
+ */
+public final class NaiveSort implements ISuffixArrayBuilder
+{
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Additional constraints enforced by this implementation:
+     * <ul>
+     * <li>{@link Integer#MIN_VALUE} must not occur in the input</li>,
+     * <li><code>input.length</code> &gt;= <code>start + length + 1</code> (to simplify
+     * border cases)</li>
+     * </ul>
+     */
+    @Override
+    public int [] buildSuffixArray(int [] input, int start, int length)
+    {
+        assertAlways(input != null, "input must not be null");
+        assertAlways(input.length >= start + length + 1, "no extra space after input end");
+
+        assert Ints.asList(input).subList(start, start + length).indexOf(
+            Integer.MIN_VALUE) < 0 : "Integer.MIN_VALUE must not occur in the input";
+
+        final int saveEOS = input[start + length];
+        try
+        {
+            input[start + length] = Integer.MIN_VALUE;
+            final int [] sa = IndirectSort.sort(start, length,
+                new SuffixComparator(input));
+            for (int i = 0; i < sa.length; i++)
+            {
+                sa[i] -= start;
+            }
+            return sa;
+        }
+        finally
+        {
+            input[start + length] = saveEOS;
+        }
+    }
+
+    /**
+     * Calculates the longest common prefix (LCP) values using naive comparison of
+     * adjecent entries in the suffix array.
+     */
+    public static int [] computeLCP(int [] input, int start, int length,
+        int [] suffixArray)
+    {
+        assert length == suffixArray.length;
+
+        final int [] lcpArray = new int [length];
+        lcpArray[0] = -1;
+        for (int i = 1; i < length; i++)
+        {
+            int lcp = 0;
+            while (input[suffixArray[i - 1] + lcp] == input[suffixArray[i] + lcp])
+            {
+                lcp++;
+            }
+            lcpArray[i] = lcp;
+        }
+
+        return lcpArray;
+    }
+}
+
+/**
+ * An indirect suffix comparator. We assume the input sequence is terminated with a unique
+ * symbol (no range checking).
+ */
+final class SuffixComparator implements IndirectComparator
+{
+    private final int [] sequence;
+
+    public SuffixComparator(int [] sequence)
+    {
+        this.sequence = sequence;
+    }
+
+    public int compare(int suffixA, int suffixB)
+    {
+        if (suffixA == suffixB) return 0;
+
+        while (sequence[suffixA] == sequence[suffixB])
+        {
+            suffixA++;
+            suffixB++;
+        }
+
+        if (sequence[suffixA] < sequence[suffixB]) return -1;
+        if (sequence[suffixA] > sequence[suffixB]) return 1;
+
+        /* Theoretically unreachable. */
+        throw new RuntimeException("Unreachable state.");
+    }
+}
